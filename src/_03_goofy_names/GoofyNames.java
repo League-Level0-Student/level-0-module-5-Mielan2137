749f31d0208c1c192d1d066fe64cae5ef80a5b0f
ilabstaff <ilabstaff@172.16.116.81>
2019-06-19 19:18:18 -0700

    Goofy Names

--- src/_03_goofy_names/GoofyNames.java
+++ src/_03_goofy_names/GoofyNames.java
@@ -8,29 +8,42 @@ import javax.swing.JOptionPane;
 
 public class GoofyNames {
 	public static void main(String[] args) {
-		String goofyName = "";
 
 		// 1. Ask the user to enter their name
 
+		String Name = JOptionPane.showInputDialog(null, "Enter Your Name: ");
+
 		// 2. Print upper case name to the console using .toUpperCase()
-		//    Run your program to see that this works.
+		// Run your program to see that this works.
 
 		// 3. Loop through each character of the name (steps 4 - 6).
-		//    HINT: Use .length() to determine the number of characters in the String.
+		// HINT: Use .length() to determine the number of characters in the String.
 
-			// 4. Create a String variable to store the next character of the name
-			//    using .substring(start, end)
-			//    HINT: replace 'start' and 'end' to get String with character at i 
-			
-			// 5. Use MODULO to set this variable to upper case for EVEN characters
-			//    and lower case for ODD characters.
+		// 4. Create a String variable to store the next character of the name
+		// using .substring(start, end)
+		// HINT: replace 'start' and 'end' to get String with character at i
 
-			
-			// 6. ADD this String  (containing 1 char) to the goofyName String
+		String goofyName = "";
 
-		
-		// 7. Use pop-up to show user their Goofy name
+		for (int i = 0; i < Name.length(); i++) {
+			if (i % 2 == 0) {
 
+				goofyName += Name.substring(i, i + 1).toUpperCase();
+				
+			} else {
+				goofyName += Name.substring(i, i + 1).toLowerCase();
+				
+				System.out.println(goofyName);
+				JOptionPane.showMessageDialog(null, goofyName);
+			}
+
+		}
 	}
 }
 
+// 5. Use MODULO to set this variable to upper case for EVEN characters
+// and lower case for ODD characters.
+
+// 6. ADD this String (containing 1 char) to the goofyName String
+
+// 7. Use pop-up to show user their Goofy name
