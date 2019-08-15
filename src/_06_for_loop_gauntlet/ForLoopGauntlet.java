67693faab231dcc06123e8d0dee707704a9b5926
ilabstaff <ilabstaff@172.16.116.76>
2019-08-14 19:29:29 -0700

    ForLoopGauntlet

---
+++ src/_06_for_loop_gauntlet/ForLoopGauntlet.java
@@ -0,0 +1,85 @@
+package _06_for_loop_gauntlet;
+
+public class ForLoopGauntlet {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 101; i++) {
+			
+				if(i%2==0) {
+					System.out.println(i);
+				
+				}
+				
+			}
+			System.out.println();
+			
+		}
+	}
+	
+
+
+
+
+
+
+//Displaying Odd Numbers
+/*
+public class ForLoopGauntlet {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 101; i++) {
+			
+				if(i%2!=0) {
+					System.out.println(i);
+				
+				}
+				
+			}
+			System.out.println();
+			
+		}
+	}
+*/
+
+
+//Displaying Even Numbers
+/*
+public class ForLoopGauntlet {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 101; i++) {
+			
+				if(i%2==0) {
+					System.out.println(i);
+				
+				}
+				
+			}
+			System.out.println();
+			
+		}
+	}
+*/
+
+
+//displaying Prime numbers
+/*public static void main(String[] args) {
+		for (int i = 0; i < 101; i++) {
+			for (int j = 2; j < i; j++) {
+				if(i%j==0) {
+					System.out.println(i+"is not a prime number");
+					break;
+				}
+				else if(j==(i-1)) {
+					System.out.println(i);
+				}
+				
+			}
+	
+}
+		
+			}
+*/
